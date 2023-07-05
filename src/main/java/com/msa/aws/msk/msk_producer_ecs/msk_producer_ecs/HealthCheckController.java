9ae0eb723191a81a85e9568dfd238ec8be768f1e
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-07-05 17:16:51 +0900

    add messagesender and webconfig

--- src/main/java/com/msa/aws/msk/msk_producer_ecs/msk_producer_ecs/HealthCheckController.java
+++ src/main/java/com/msa/aws/msk/msk_producer_ecs/msk_producer_ecs/HealthCheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
-    @GetMapping("/healthCheck")
+    @GetMapping("/msk-producer-healthCheck")
         public ResponseEntity<String> healthCheck() {
             return ResponseEntity.ok("Healthy");
     }
