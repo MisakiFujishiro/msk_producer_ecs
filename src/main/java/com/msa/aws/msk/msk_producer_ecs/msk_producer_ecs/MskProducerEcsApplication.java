d0030b156d8167f7b0e6d0347a5487ed83aa5354
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-07-05 14:11:49 +0900

    initial Commit

---
+++ src/main/java/com/msa/aws/msk/msk_producer_ecs/msk_producer_ecs/MskProducerEcsApplication.java
@@ -0,0 +1,13 @@
+package com.msa.aws.msk.msk_producer_ecs.msk_producer_ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MskProducerEcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MskProducerEcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msa/aws/msk/msk_producer_ecs/msk_producer_ecs/MskProducerEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.aws.msk.msk_producer_ecs.msk_producer_ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MskProducerEcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
