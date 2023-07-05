673f721388a44cbf78e11f450de76edcad29721e
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-07-05 15:18:55 +0900

    add main for mirroring

---
+++ src/main/java/com/msa/aws/msk/msk_producer_ecs/msk_producer_ecs/frontendController.java
@@ -0,0 +1,12 @@
+package com.msa.aws.msk.msk_producer_ecs.msk_producer_ecs;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class frontendController {
+    @RequestMapping(value="/msk-producer", produces = "text/plain")
+    public String frontend() {
+        return "Hello msk-producer-ecs!";
+    }
+}
+
